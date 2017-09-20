e4bc0772678b13f50876d640ffb59a1382990b51
Doc813 <32145390+Doc813@users.noreply.github.com>
2017-09-20 23:51:26 +0300

    Add files via upload

---
+++ Lab1.6_Golubeva.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+main()
+{
+int num;
+char ch;
+scanf("%d\n",&num); //ввод числа/
+scanf("%c",&ch); //ввод символа/
+printf("%d\n", num); //вывод числа/
+printf("%c",ch);//вывод символа/
+}
