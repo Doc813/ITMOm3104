51169edd618db82490f7e211d96fa47614b9b303
Doc813 <32145390+Doc813@users.noreply.github.com>
2017-09-20 23:52:21 +0300

    Add files via upload

---
+++ Lab2.6_Golubeva.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <math.h> 
+main()
+{
+	float a;
+scanf("%lf", &a);
+float z1,z2;
+z1==cos(a)+cos(2*a)+cos(6*a)+cos(7*a);
+z2==4*cos(a/2)*cos(5/2*a)*cos(4*a);
+printf("%lf\n", z1);
+printf("%lf",z2);
+}
